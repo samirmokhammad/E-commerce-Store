d352ed751749b981cfc16db262f29a5e2d721c4f
Samir <samirmokhammad13@gmail.com>
2026-04-03 20:38:52 +0200

    chore: initialize project with package.json and Vite configuration for React

---
+++ components/App.jsx
@@ -0,0 +1,16 @@
+import Heading from './Heading';
+import { Outlet } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <>
+            <Heading />
+
+            <main>
+                <Outlet />
+            </main>
+        </>
+    );
+};
+
+export default App;
---
+++ main.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import App from './components/App';
+
+createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />}></Route>
+        </Routes>
+    </BrowserRouter>,
+);
